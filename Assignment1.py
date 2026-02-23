43c8027a112d219586516da0e67a9a6c980d8883
Mehtab Alam <mehtabalamnew@gmail.com>
2026-02-23 17:11:08 +0530

    hash function and handling the url for wikipedia like urls

--- Assignment1.py
+++ Assignment1.py
@@ -2,20 +2,62 @@ from bs4 import BeautifulSoup
 
 import requests
 
-url = input("Enter your url: ")
+import sys 
 
-result = requests.get(url)
+
+
+url = sys.argv[1]
+
+headers = {
+    'User-Agent': 'ScrapingBee/1.0 (https://www.scrapingbee.com; support@scrapingbee.com)'   
+}
+
+result = requests.get(url, headers=headers)
+
+# result = requests.get(url)
 # tag1 = doc.title
 
 # print(tag.String)
 
-
-
 doc = BeautifulSoup(result.text,"html.parser")
 # print(doc.prettify())
 
-print(doc.title.string)
-print(doc.body.get_text())
+if doc.title:
+    print(doc.title.string)
+else:
+    pass
+if doc.body:
+    print(doc.body.get_text())
+else:
+    pass
+
+for urls in doc.find_all("a"):
+    href = urls.get("href")
+    if href:
+        print(href)
+
+
+
+word_Dict = {}
+def countBodyCharacter(body_content):
+    for word_s in body_content.split():
+        if word_s.lower() in word_Dict:
+            word_Dict[word_s.lower()] = word_Dict[word_s.lower()] + 1
+        else:
+            word_Dict[word_s.lower()] = 1
+    print(word_Dict)
+
+
+res = countBodyCharacter(doc.body.get_text())
+print(res)
+
+
+
+
+
+
+
+
 
 
 
