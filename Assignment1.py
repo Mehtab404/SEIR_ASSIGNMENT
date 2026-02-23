5735f648e55faacb2b0dc707c9cc7b3996279c3c
Mehtab Alam <mehtabalamnew@gmail.com>
2026-02-23 22:12:11 +0530

    updates a little bit

--- Assignment1.py
+++ Assignment1.py
@@ -38,70 +38,16 @@ for urls in doc.find_all("a"):
 
 
 
-word_Dict = {}
-def countBodyCharacter(body_content):
-    for word_s in body_content.split():
-        stop_words = ["is","am","are","the","not","in","on","or","but","if","while","at","for","by"]
-        if word_s in stop_words: continue
-        if word_s.lower() in word_Dict:
-            word_Dict[word_s.lower()] = word_Dict[word_s.lower()] + 1
-        else:
-            word_Dict[word_s.lower()] = 1
-    print(word_Dict)
-
-
-res = countBodyCharacter(doc.body.get_text())
-print(res)
 
 
 
-def hashCode(words):
-    p = 53
-    m = 2**64
-
-    s = 0
-    final  =0
-    for i in words:
-        final = (final + ord(i)*(p**s))%m
-        s+=1
-    return final
 
 
 
-def simhash(word_list):
-    sim = [0]*64
-    for j in word_list:
-        hash = hashCode(j)
-        for i in range(64):
-            if (hash >> i) & 1:
-                sim[i] += word_list[j]
-            else:
-                sim[i] -= word_list[j]
-    
-    fingerprint = 0
-    for i, v in enumerate(sim):
-        if v > 0:
-            fingerprint = fingerprint | (1 << i)
-
-    return fingerprint
-
-def findcommonbits(fingerprint1, fingerprint2):
-    temp = fingerprint1 ^ fingerprint2
-    count = 0
-    while temp:
-        bit = temp & 1
-        if bit == 1:
-            count += 1
-        temp >>= 1
-    diff = 64 - count
-    return diff
 
 
-"""
-url1 -> html1 -> body1 -> fingerprint1
-url2 -> html2 -> body2 -> fingerprint2
-common -> 
-"""
+
+        
 
 
 
